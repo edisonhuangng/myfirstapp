f249f8000d7346d6c1d9710931a33bd586bd009a
edisonhuang <edisonhuang.ng@gmail.com>
2018-05-15 14:51:42 +0800

    v1

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myfirstappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myfirstappApp
+ */
+angular.module('myfirstappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
